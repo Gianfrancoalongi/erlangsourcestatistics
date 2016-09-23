01f66fa078fcec2a7090e52be7827c2fc8f564d1
Peter Lundell <peter.lundell@ericsson.com>
2016-09-23 08:29:14 +0200

    Clean up a bit

--- test/test_dir/file_read_test.erl
+++
@@ -1,9 +0,0 @@
--module(file_read_test).
--export([f/0,g/0]).
-
-f() ->
-    1,
-    2.
-
-g() ->
-    1.
--- test/test_dir/test_dir/a.erl
+++
@@ -1,5 +0,0 @@
--module(a).
--export([function/1]).
-
-function(Argument) ->
-    Argument + 1.
